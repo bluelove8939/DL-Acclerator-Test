473cf47d8874b3a65f8e2f18c6262b074bcf48dd
bluelove8939 <bluelove8939@naver.com>
2022-08-19 18:43:41 +0900

    Initial Commit

---
+++ onchip_buffer_compression_test.py
@@ -0,0 +1,54 @@
+import os
+import argparse
+import configparser
+
+from scalesim.scale_sim import scalesim
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', metavar='Topology file', type=str,
+                        default=os.path.join(os.curdir, 'topologies', 'conv_nets', 'alexnet.csv'),
+                        help="Path to the topology file"
+                        )
+    parser.add_argument('-c', metavar='Config file', type=str,
+                        default=os.path.join(os.curdir, 'configs', 'google.cfg'),
+                        help="Path to the config file"
+                        )
+    parser.add_argument('-p', metavar='log dir', type=str,
+                        default='auto',
+                        help="Path to log dir"
+                        )
+
+    args = parser.parse_args()
+    topology = args.t
+    temppath = args.c
+    logpath = args.p
+    if logpath == 'auto':
+        logpath = os.path.join(os.curdir, 'test_runs',
+                               f'buffersize_test_'
+                               f'{temppath.split(os.sep)[-1].split(".")[0]}_'
+                               f'{topology.split(os.sep)[-1].split(".")[0]}')
+
+    os.makedirs(logpath, exist_ok=True)
+
+    buffsizes = [16, 32, 64, 256]
+
+    for bf in sorted(buffsizes, reverse=True):
+        config = configparser.ConfigParser()
+        config.read(temppath)
+        config['general']['run_name'] = '_'.join([config['general']['run_name'], f"bf_{bf}"])
+        config['architecture_presets']['IfmapSramSzkB'] = str(bf)
+        config['architecture_presets']['FilterSramSzkB'] = str(bf)
+        config['architecture_presets']['OfmapSramSzkB'] = str(bf)
+        config['run_presets']['InterfaceBandwidth'] = 'USER'
+        
+        config_filepath = os.path.join(os.curdir, 'configs', '.'.join([config['general']['run_name'], 'cfg']))
+        with open(config_filepath, 'wt') as file:
+            config.write(file)
+
+        s = scalesim(save_disk_space=True, verbose=True,
+                 config=config_filepath,
+                 topology=topology,
+                 )
+        s.run_scale(top_path=logpath)
